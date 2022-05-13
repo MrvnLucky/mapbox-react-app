cdbd7cf0752133ba3775ac287a24530b56a1731c
Marvin <marvnluck@gmail.com>
2022-05-14 01:47:36 +0700

    disable no-webpack-loader

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
-import mapboxgl from "mapbox-gl";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
 import "./Map.css";
 
